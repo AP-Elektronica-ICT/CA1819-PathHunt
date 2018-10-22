4cee35badcca0fa75d59c12cb128c25a96283778
UberGhost1161 <33574301+UberGhost1161@users.noreply.github.com>
2018-10-22 10:27:03 +0200

    Basic TeamBuilder layout + onclicklistner btn

--- Frontend/PathHunt/app/src/main/java/com/example/pathhunt/pathhunt/TeamBuilder.java
+++ Frontend/PathHunt/app/src/main/java/com/example/pathhunt/pathhunt/TeamBuilder.java
@@ -2,12 +2,27 @@ package com.example.pathhunt.pathhunt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TeamBuilder extends AppCompatActivity {
+
+    private Button BtnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_builder);
+
+        BtnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BtnContinueClicked();
+            }
+        });
+    }
+
+    private void BtnContinueClicked(){
+        =
     }
 }
