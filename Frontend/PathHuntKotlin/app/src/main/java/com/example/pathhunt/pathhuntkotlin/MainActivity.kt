5544c9eaea8de9ea10c342bedd67cf9a7f889d04
UberGhost1161 <33574301+UberGhost1161@users.noreply.github.com>
2018-11-11 17:34:02 +0100

    Kotlin version of teambuilder

--- Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/MainActivity.kt
+++ Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.pathhunt.pathhuntkotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCreateTeam.setOnClickListener{
+            val intent = Intent(this, TeamNameEditor::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/TeamEditor.kt
@@ -0,0 +1,14 @@
+package com.example.pathhunt.pathhuntkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_team_editor.*
+
+class TeamEditor : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team_editor)
+        NrPick.maxValue
+    }
+}
