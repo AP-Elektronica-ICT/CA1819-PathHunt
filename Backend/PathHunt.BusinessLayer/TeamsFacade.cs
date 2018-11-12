9393b8a1d4520a0d3868b6d17921ef1aae82bbde
UberGhost1161 <33574301+UberGhost1161@users.noreply.github.com>
2018-11-12 19:42:32 +0100

    First part backend

---
+++ Backend/PathHunt.BusinessLayer/TeamsFacade.cs
@@ -0,0 +1,26 @@
+﻿using PathHunt.DataLayer;
+using PathHunt.DataLayer.Model;
+using System;
+using System.Linq;
+
+namespace PathHunt.BusinessLayer
+{
+    public class TeamsFacade
+    {
+        private readonly GameContext context;
+
+        public TeamsFacade(GameContext _context)
+        {
+            this.context = _context;
+        }
+        public List<Team> GetAllTeams()
+        {
+            return context.Teams.ToList();
+        }
+        public Team GetTeam(int id)
+        {
+            return context.Questions
+                .SingleOrDefault(d => d.Id == id);
+        }
+    }
+}
