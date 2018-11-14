09a24b2b25980f59f7d7f1037d21f2231109d386
MrLehert <marino_jo@hotmail.com>
2018-11-14 11:22:07 +0100

    CA18PH-51: added location permission & current location

--- Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/MapsActivity.kt
+++ Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/MapsActivity.kt
@@ -1,7 +1,9 @@
 package com.example.pathhunt.pathhuntkotlin
 
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,6 +16,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    //this companion object will ask for permission to use locationservices
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
@@ -21,6 +28,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        setUpMap()
+
     }
 
 
@@ -32,9 +41,24 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(school).title("Marker on the school"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(school))
 
+        /*You can choose a different maptypes with this fct*/
         mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+        //sets zoomcontrols
         mMap.uiSettings.isZoomControlsEnabled = true
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(school, 14.0f))
 
+        //this shows current location on map, uses the checkselfpermission to check if location services are allowed, if not allowed currentlocation will not show
+        mMap.isMyLocationEnabled = true
+
+    }
+
+    //checks for permission to search for finelocation (currentlocation)
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
     }
 }
