42b5bc17e794dd03312e0e293fe72cfbacffe262
UberGhost1161 <33574301+UberGhost1161@users.noreply.github.com>
2018-11-14 11:21:41 +0100

    Added post function in app

--- Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/Team.kt
+++ Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/Team.kt
@@ -1,7 +1,6 @@
 package com.example.pathhunt.pathhuntkotlin
 
 data class Team(
-    val id: Int,
     val name: String,
     val score: Int
 )
--- Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/TeamNameEditor.kt
+++ Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/TeamNameEditor.kt
@@ -3,6 +3,11 @@ package com.example.pathhunt.pathhuntkotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_team_name_editor.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
+import java.net.URL
+import khttp.post
 
 
 class TeamNameEditor : AppCompatActivity() {
@@ -11,16 +16,19 @@ class TeamNameEditor : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team_name_editor)
         btnContinue.setOnClickListener {
-            val teamname = etxtTeamname.text.toString()
 
             /*val intent = Intent(this, TeamEditor::class.java)
             intent.putExtra("Name", teamname)
             startActivity(intent)*/
+
+            CreateTeam()
             finish()
         }
     }
 
-    private fun CreateTeam(team: Team) {
+    private fun CreateTeam() {
         val serverURL: String = "http://192.168.1.37:45455/api/teams"
+
+        post(serverURL, data = Team(name = etxtTeamname.text.toString(), score = 0))
     }
 }
