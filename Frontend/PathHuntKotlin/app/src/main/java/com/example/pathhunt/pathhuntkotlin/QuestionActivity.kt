4cd88abe7ced9b69f4f02abd90fab31a1c1a16f9
Stefan <stefan.vanderveeken@gmail.com>
2018-11-26 19:29:00 +0100

    Conveyor tool beschrijving + Fuel library implementatie

--- Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/QuestionActivity.kt
+++ Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/QuestionActivity.kt
@@ -5,13 +5,18 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.beust.klaxon.Klaxon
+import com.github.kittinunf.fuel.android.extension.responseJson
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.rx.rx_object
+import com.github.kittinunf.result.Result
+import com.github.kittinunf.result.getAs
 import kotlinx.android.synthetic.main.activity_question.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import java.net.URL
 
 class QuestionActivity : AppCompatActivity() {
-    var question : Question? = null
+    var allquestions : MutableList<Question?>? = null
     var answer : String? = null
     var userAnswer : String? = null
     var QuestionId : Int = 1
@@ -56,17 +61,42 @@ class QuestionActivity : AppCompatActivity() {
     }
 
     fun getInfo(id: Int){
-        doAsync {
-            val result = URL("http://172.16.193.233:45455/api/questions/$id").readText()
-            question = Klaxon()
-                .parse<Question>(result)
-            //Log.d("log1", result)
-            uiThread {
-                if (question != null) {
-                    answer = question?.answer.toString();
-                    txtQuestion.text = question?.content.toString()
-                }
+        var data: Question? = null
+        val url = "http://192.168.1.62:45455/api/questions"
+        url.httpGet().responseObject(Question.Deserializer()){request, response, result ->
+            val (questions, err) = result
+            questions?.forEach { question ->
+                Log.d("Questions","Content ${question.content}, antwoord is ${question.answer}")
+                allquestions?.add(question)
             }
+            Log.d("All Questions", allquestions.toString())
         }
+//        "http://192.168.137.1:45455/api/questions/1".httpGet().responseString(){request, response, result ->
+//            when(result){
+//                is Result.Success ->{
+//                    data = Klaxon().parse<Question>(result.get())
+//                    Log.d("LogResult", result.get())
+//                    answer = data?.answer.toString()
+//                    txtQuestion.text = data?.content.toString();
+//                }
+//
+//                is Result.Failure ->{
+//                    txtQuestion.text = "Couldn't find question"
+//                }
+//            }
+
+ //       }
+//        doAsync {
+//            val result = URL("http://192.168.137.1:45455/api/questions/$id").readText()
+//            question = Klaxon()
+//                .parse<Question>(result)
+//            //Log.d("log1", result)
+//            uiThread {
+//                if (question != null) {
+//                    answer = question?.answer.toString();
+//                    txtQuestion.text = question?.content.toString()
+//                }
+//            }
+//        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 CA1819-PathHunt
+
+# Conveyor by Keyoti
+Wij hebben de tool 'Conveyor' gebruikt om IIS Express bereikbaar te maken op het lokale netwerk zodat we onze web applicatie gemakkelijk kunnen testen vanaf een andere computer of smartphone. De stappen die je moet ondernemen om de tool te gebruiken zijn als volgt:
+
+Link: https://marketplace.visualstudio.com/items?itemName=vs-publisher-1448185.ConveyorbyKeyoti
+
+* Download de Visual Studio Exentension door Conveyor op te zoeken in de toolbar bij Tools -> Extensions and Updates <br>
+* Je moet een regel voor binnenkomende verbindingen toevoegen in je firewall om toegang tot de TCP poort van Remote URL te geven.
+    * Zoek bij windows naar 'WF.msc'
+    * Klik op 'regels voor binnenkomende verbindingen'
+    * Klik op 'nieuwe regel'
+    * Klik op 'Poort' en dan op volgende
+    * Selecteer TCP en zet hier vervolgens de poort van de Remote URL in het vak 'Specifieke lokale poorten'. Dit is waarschijnlijk poort 45455, druk dan op volgende. Deze kan je ook vinden door je web applicatie te starten wanneer conveyor is geïnstalleerd. Conveyor wordt automatisch gestart voor web applicaties.
+    * Druk dan op volgende en vervolgens nog eens op volgende (Je kan 'Public' uitzetten), en geef het vervolgens een naam bv. 'Conveyor: web dev server access enabled'.
+* Zoek vervolgens in het bestand 'applicationhost.config', dat standaard in de map Documenten/IISExpress/config te vinden is, naar de volgende regel:  
+<binding protocol "http" bindingInformation=":8080:localhost"> en verander het in <binding protocol="http" bindingInformation="*:8080:*" />
+* Start Visual Studio nu op met administrator rechten en nu zou je het IP met de bijbehorende poort (Waarschijnlijk 45455) van je desktop/laptop moeten kunnen ingeven om je applicatie te bereiken.
