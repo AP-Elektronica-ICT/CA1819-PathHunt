52ec1ef4c848cf1fc71f159ab5f621933dae268c
SanderVOverloop <33574301+SanderVOverloop@users.noreply.github.com>
2018-11-27 21:05:36 +0100

    Post request working

--- Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/Team.kt
+++ Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/Team.kt
@@ -1,29 +1,35 @@
 package com.example.pathhunt.pathhuntkotlin
 
 import com.github.kittinunf.fuel.core.ResponseDeserializable
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.httpPost
 import com.google.gson.Gson
 
-/*interface Team {
-    val teamname: String
-    val score: Int
-    //val email: String
-}
-
-data class getTeam(val id: Int,
-                   override val teamname: String,
-                   override val score: Int): Team
-
-data class  postTeam(override val teamname: String,
-                     override val score: Int = 0):Team*/
-
 data class Team (
-    val id: Int,
+    val id: Int?,
     val name: String,
     val score: Int
 ){
     class Deserializer: ResponseDeserializable<Array<Team>>{
         override fun deserialize(content: String): Array<Team>? {
             return  Gson().fromJson(content, Array<Team>::class.java)
+        }
+    }
+}
+
+public fun CreateTeam(URL: String ,team: Team) {
+    URL.httpPost()
+        .jsonBody(Gson().toJson(team))
+        .response {_,_, result ->
+            println("result: " + result.toString())
+        }
+}
+
+public fun GetTeam(URL: String) {
+    URL.httpGet().responseObject(Team.Deserializer()) { request, response, result ->
+        val (teams, err) = result
+        teams?.forEach { team ->
+            println("Team: ${team.name}")
         }
     }
 }
--- Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/TeamNameEditor.kt
+++ Frontend/PathHuntKotlin/app/src/main/java/com/example/pathhunt/pathhuntkotlin/TeamNameEditor.kt
@@ -1,15 +1,10 @@
 package com.example.pathhunt.pathhuntkotlin
 
-import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.widget.Toast
-import com.github.kittinunf.fuel.httpGet
-import com.github.kittinunf.fuel.httpPost
 import kotlinx.android.synthetic.main.activity_team_name_editor.*
-import javax.xml.transform.Result
 
+val URL: String = "http://192.168.1.37:45455/api/teams"
 
 class TeamNameEditor : AppCompatActivity() {
 
@@ -18,55 +13,10 @@ class TeamNameEditor : AppCompatActivity() {
         setContentView(R.layout.activity_team_name_editor)
 
         btnContinue.setOnClickListener {
+            val team = Team(null, etxtTeamname.text.toString(), 0)
 
-            /*val intent = Intent(this, TeamEditor::class.java)
-            intent.putExtra("Name", teamname)
-            startActivity(intent)*/
-
-
-
-
-            //val jsonString = Gson().toJson(team) as String
-
-            //Toast.makeText(this, team.teamname, Toast.LENGTH_LONG).show()
-
-            //Log.d("JSONTag", etxtTeamname.text.toString())
-
-            //CreateTeam(team)
-
-            GetTeam()
-
+            CreateTeam(URL, team)
             finish()
         }
-    }
-
-    private fun CreateTeam(team: Team) {
-
-    }
-
-    private fun GetTeam() {
-
-        val URL: String = "http://192.168.1.37:45455/api/teams"
-
-        var names: String = ""
-
-        URL.httpGet().responseObject(Team.Deserializer()) { request, response, result ->
-            val (teams, err) = result
-            teams?.forEach { team ->
-                println("Team: ${team.name}")
-            }
-            //MakeToast(names)
-        }
-        /*URL.httpGet().responseString(){request, response, result ->
-            when(result){
-                is Result.Success -> { println("Result: ${result.get()}")}
-                is Result.Failure -> {}
-            }
-        }*/
-
-    }
-
-    private fun MakeToast(msg: String) {
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
 }
